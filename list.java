ad22284bea27f9817d86c64bcaf65d03393a2ee1
xiyuwuwang <126329295+xiyuwuwang@users.noreply.github.com>
2023-06-29 18:00:20 +0800

    Update list.java

--- list.java
+++ list.java
@@ -11,7 +11,16 @@ public class TestBt {
         String s = "^[\\-\\+]?(0(\\.\\d{1,8})?|([1-9](\\d)?)(\\.\\d{1,8})?|1[0-7]\\d{1}(\\.\\d{1,8})?|180(([.][0]{1,8})?))$";
 
         boolean matches = Pattern.matches(s, "120.6811124");
-        System.out.println(matches);
+        System.out.println(matches);        
+    }
+        //获取某个时间段每天的日期
+    @Test
+    public void test01() {
+    LocalDateTime min = LocalDateTime.now().with(LocalTime.MIN);
+    LocalDateTime max = LocalDateTime.now().plusDays(10).with(LocalTime.MAX);
+   List<LocalDateTime> collect = Stream.iterate(min, localDate -> localDate.plusDays(1))
+            .limit(ChronoUnit.DAYS.between(min, max) + 1)
+            .collect(Collectors.toList());
     }
 
     }
