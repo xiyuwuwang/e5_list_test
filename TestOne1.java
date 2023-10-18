55c7043c0c8782068dc9f3b85d81675656f3025e
xiyuwuwang <126329295+xiyuwuwang@users.noreply.github.com>
2023-10-18 16:36:10 +0800

    Create TestOne1.java

---
+++ TestOne1.java
@@ -0,0 +1,9 @@
+public class TestOne1 {
+    @Test
+    public void test1() {
+
+LockSupport.unpark(caffeineOnUpMsg.getIfPresent(requestId));
+
+      LockSupport.parkUntil(System.currentTimeMillis() + 10000);
+}
+}
